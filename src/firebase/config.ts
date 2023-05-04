29dc4d2dec8f91e21ab79bf797a18b0bead01ae4
balaturgija <matej.balat@gmail.com>
2023-05-04 12:44:35 +0200

    Add firebase config

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,1 +1,23 @@
 import * as admin from "firebase-admin";
+// import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: String(process.env.FIREBAE_API_KEY),
+  authDomain: String(process.env.AUTH_DOMAIN),
+  projectId: String(process.env.PROJECT_ID),
+  storageBucket: String(process.env.STORAGE_BUCKET),
+  messagingSenderId: String(process.env.MESSAGING_SENDER_ID),
+  appId: String(process.env.APP_ID),
+};
+
+// Initialize Firebase
+
+admin.initializeApp({
+  credential: admin.credential.cert(firebaseConfig),
+});
+
+admin.firestore().settings({ ignoreUndefinedProperties: true });
---
+++ src/firebase/firebase.service.ts
@@ -0,0 +1,5 @@
+import * as functions from "firebase-functions";
+import { injectable } from "tsyringe";
+
+@injectable()
+export class FirebaseService {}
