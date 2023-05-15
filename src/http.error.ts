c56fe0a155ba64408233a57ebc2b32e0386ab098
balaturgija <matej.balat@gmail.com>
2023-05-15 20:46:37 +0200

    Remove property

--- src/http.error.ts
+++ src/http.error.ts
@@ -2,7 +2,6 @@ import { ValidationError } from "class-validator";
 export class HttpException {
   statusCode: number;
   message: string | ValidationError[] | string[];
-  path?: string;
 
   constructor(
     statusCode: number = 500,
