2e9be9ec9b49cfb34c21e5fde497d533c83c2817
balaturgija <matej.balat@gmail.com>
2023-05-15 20:45:25 +0200

    Create car module

---
+++ src/cars/modules/car.module.ts
@@ -0,0 +1,23 @@
+import { Engine } from "../../engines/modules/engine.module";
+
+export interface Car {
+  id: string;
+  brand: string;
+  model: string;
+  kilometers: number;
+  engine: Engine;
+  createdAt: Date;
+  updatedAt: Date | null;
+}
+
+export class Car {
+  constructor(data: FirebaseFirestore.DocumentData | undefined) {
+    this.id = data.id;
+    this.brand = data.brand;
+    this.model = data.model;
+    this.kilometers = data.kilometers;
+    this.engine = data.engine;
+    this.createdAt = data.createdAt.toDate();
+    this.updatedAt = data.updatedAt === null ? null : data.updatedAt.toDate();
+  }
+}
