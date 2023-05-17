93a4377f10d42e2bd7115304499c738e8ae0ac9e
balaturgija <matej.balat@gmail.com>
2023-05-17 14:11:39 +0200

    Remove route

--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,10 @@
 import { Router } from "express";
 import { carsRouter } from "./cars/cars.routes";
 import { enginesRouter } from "./engines/engines.routes";
-import { proposlasRouter } from "./proposals/proposlas.routes";
 
 const router = Router();
 
 router.use("/cars", carsRouter);
 router.use("/engines", enginesRouter);
-router.use("/proposals", proposlasRouter);
 
 export default router;
