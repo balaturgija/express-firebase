dc6d50e171a28a44aa7753d596518dca575181d8
balaturgija <matej.balat@gmail.com>
2023-05-17 14:13:46 +0200

    Fix typo

--- src/middlewares/engine-check.middleware.ts
+++ src/middlewares/engine-check.middleware.ts
@@ -1,11 +1,11 @@
 import { NextFunction, Request } from "express";
 import { Response } from "firebase-functions/v1";
 import { EnginesService } from "../engines/engines.service";
-import { EnginesRepsoitory } from "../engines/enignes.repository";
+import { EnginesRepository } from "../engines/enignes.repository";
 
 export const engineCheck = () => {
   return async (req: Request, response: Response, next: NextFunction) => {
-    const engineService = new EnginesService(new EnginesRepsoitory());
+    const engineService = new EnginesService(new EnginesRepository());
     try {
       let engineId;
       if (req.body.engineId) engineId = req.body.engineId;
